610119c0759c40b29eaf0be3a801177db58e4b36
N Diaye C Abdul <abdul13un@gmail.com>
2025-05-19 20:04:54 +0200

    Commit initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import NotFound from './pages/notFound/NotFound';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
